e72604c8cf26c57eb69de9f1f046f9a08ab07e8f
Haroon Awan <39236904+haroonawanofficial@users.noreply.github.com>
2019-05-27 08:37:06 +0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,3 +1,23 @@
+# *************************************************************************************** #
+# ---------------------------------- EULA NOTICE ---------------------------------------- #
+#                     Agreement between "Haroon Awan" and "You"(user).                    #
+# ---------------------------------- EULA NOTICE ---------------------------------------- #
+#  1. By using this piece of software your bound to these point.                          #
+#  2. This an End User License Agreement (EULA) is a legal between a software application #
+#     author "Haroon Awan" and (YOU) user of this software.                               #
+#  3. This software application grants users rights to use for any purpose or modify and  #
+#     redistribute creative works.                                                        #
+#  4. This software comes in "is-as" warranty, author "Haroon Awan" take no responsbility #
+#     what you do with by/this software as your free to use this software.                #
+#  5. Any other purpose(s) that it suites as long as it is not related to any kind of     #
+#     crime or using it in un-authorized environment.                                     #
+#  6. You can use this software to protect and secure your data information in any        #
+#     environment.                                                                        #
+#  7. It can also be used in state of being protection against the unauthorized use of    #
+#     information.                                                                        #
+#  8. It can be used to take measures achieve protection.                                 #
+# *************************************************************************************** #
+
 #!/bin/bash
 
 red="\e[0;31m"
@@ -7,30 +27,20 @@ off="\e[0m"
 function banner() {
 clear
 echo "                                                                                               ";
-echo " .########..########..######...#######..##....##.... ######...#...#####...#####...#######..	 "; 
-echo " .##.....##.##.......##....##.##.....##.###...##....##......##.##.##. ##..##..##..##...##...	 "; 
-echo " .##.....##.##.......##.......##.....##.####..##....##......##.##.##.##   ##..##. ##...##......	 "; 
-echo " .########..######...##.......##.....##.##.##.##....##......##.##.##. ##..##.##...#######.	 "; 
-echo " .##...##...##.......##.......##.....##.##..####....##......##.##.##.  ##.##..##..##...##...	 "; 
-echo " .##....##..##.......##....##.##.....##.##...###....##......##.##.##.##...##...##.##...##...	 "; 
-echo " .##.....##.########..######...#######..##....##.....######.. #.....#.....##....#.##...##......	 ";  
-echo "                                                                                               ";   
+echo "                                                                                               ";
+echo "                                                                                               ";
+echo "        .########...########..######.....#######...##....##.....######.....#######....######....########......########..      "; 
+echo "         .##.....##..##.......##....##..##.....##..###...##....##....##...##.....##...##...##...##.....##....##.....##.	 "; 
+echo "         .##.....##..##.......##........##.....##..####..##....##.........##.....##...##..##....##......##...##.....##.	 "; 
+echo "         .########...######...##........##.....##..##.##.##....##.........##.....##...##..##....#########....#########.	 "; 
+echo "         .##...##....##.......##........##.....##..##..####....##.........##.....##...##...##...##....##.....##.....##.	 "; 
+echo "         .##....##...##.......##....##..##.....##..##...###....##....##...##.....##...##...###..##.....##....##.....##.	 "; 
+echo "         .##.....##..########..######....#######...##....##.....######.....#######....#######...##......##...##.....##.	 ";  
+echo "                                       	  Ultimate Recon and Foot Printing Software     Version 1.0a        ";   
+echo "                                                                                               ";
+echo "                                                                                               ";
+echo "                                                                                               ";
 }
-
-
-echo "y" | apt-get install xdg-utils
-echo "y" | apt-get install hping3
-echo "y" | apt-get install python3
-echo "y" | git clone https://github.com/haroonawanofficial/cobra.git
-echo "y" | git clone https://github.com/exiftool/exiftool.git
-echo "y" | git clone https://github.com/sensepost/BiLE-suite.git
-echo "y" | git clone https://github.com/haroonawanofficial/ShaheenX.git
-echo "y" | git clone https://github.com/haroonawanofficial/maahro.git
-echo "y" | git clone https://github.com/haroonawanofficial/panthera.git
-echo "y" | git clone https://github.com/FortyNorthSecurity/EyeWitness.git
-cd EyeWitness/setup
-chmod u+x setup.sh
-./setup.sh
 
 function linux() {
 echo -e "$red [$green+$red]$off Installing Perl ...";
@@ -74,6 +84,21 @@ perl /usr/share/ReconCobra/ReconCobra.pl" '${1+"$@"}' > "reconcobra";
     sudo cp "config/ReconCobra.desktop" "/usr/share/applications"
     sudo cp "reconcobra" "/usr/local/bin/"
     rm "reconcobra";
+
+echo "y" | apt-get install xdg-utils
+echo "y" | apt-get install hping3
+echo "y" | apt-get install python3
+echo "y" | git clone https://github.com/haroonawanofficial/cobra.git
+echo "y" | git clone https://github.com/exiftool/exiftool.git
+echo "y" | git clone https://github.com/sensepost/BiLE-suite.git
+echo "y" | git clone https://github.com/haroonawanofficial/ShaheenX.git
+echo "y" | git clone https://github.com/haroonawanofficial/maahro.git
+echo "y" | git clone https://github.com/haroonawanofficial/panthera.git
+echo "y" | git clone https://github.com/FortyNorthSecurity/EyeWitness.git
+cd EyeWitness/setup
+chmod u+x setup.sh
+./setup.sh
+
 
 if [ -d "/usr/share/ReconCobra" ] ;
 then
