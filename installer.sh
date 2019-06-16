ce58a08eaf960d3de517e93c2f11152dde100ea0
Haroon Awan <39236904+haroonawanofficial@users.noreply.github.com>
2019-06-16 21:01:08 +0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -95,7 +95,7 @@ echo "y" | git clone https://github.com/haroonawanofficial/ShaheenX.git
 echo "y" | git clone https://github.com/yassineaboukir/Asnlookup.git
 echo "y" | git clone https://github.com/exiftool/exiftool.git
 echo "y" | git clone https://github.com/sensepost/BiLE-suite.git
-echo "y" | https://github.com/haroonawanofficial/vasl.git
+echo "y" | git clone https://github.com/haroonawanofficial/vasl.git
 echo "y" | git clone https://github.com/haroonawanofficial/panthera.git
 echo "y" | git clone https://github.com/naqushab/SearchEngineScrapy.git
 echo "y" | apt-get install nmap
