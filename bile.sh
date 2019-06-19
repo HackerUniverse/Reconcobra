b1054d80d09f47bfc30922f20fb690e85f50f638
Haroon Awan <39236904+haroonawanofficial@users.noreply.github.com>
2019-06-19 20:04:53 +0500

    Update bile.sh

--- bile.sh
+++ bile.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+cd BiLE-suite
 echo "Enter Target"
 read Target
 perl bile-weigh.pl $Target sp_bile_out.txt.mine out.txt 
 perl bile-weigh.pl $Target sp_bile_out.txt.mine out.txt
+cd ..
