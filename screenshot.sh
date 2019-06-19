3e15364fd2881ea98c9e01c01b9785b90874787c
Haroon Awan <39236904+haroonawanofficial@users.noreply.github.com>
2019-06-19 19:59:05 +0500

    Update screenshot.sh

--- screenshot.sh
+++ screenshot.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "Hint: Do Port Scan before using this tool"
-eyewitness -f /root/urls.txt -d screens --headless --active-scan --add-http-ports '21,25,80,137,139,443,445,8000,8080,8090,9000,9001,8443,3389,3306' --add-https-ports '21,25,80,137,139,443,445,8000,8080,8090,9000,9001,8443,3389,3306'
+echo "Hint: Do Net Block Scan before using this tool"
+cd EyeWitness
+python EyeWitness -f /root/urls.txt -d screens --headless --active-scan --add-http-ports '21,25,80,137,139,443,445,8000,8080,8090,9000,9001,8443,3389,3306' --add-https-ports '21,25,80,137,139,443,445,8000,8080,8090,9000,9001,8443,3389,3306'
+cd ..
