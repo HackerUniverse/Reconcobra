d367abc0e8e5fac4210e429cf266a3aca8536c50
Haroon Awan <39236904+haroonawanofficial@users.noreply.github.com>
2019-06-19 12:35:00 +0500

    Update cob_b.sh

--- cob_b.sh
+++ cob_b.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
 wget -i metacrawler_googlefiles.txt
-cd exiftool
-./exiftool *.pdf > pdfmetacrawler_results.txt
-./exiftool *.svg > svgmetacrawler_results.txt
-./exiftool *.doc > docmetacrawler_results.txt
-./exiftool *.xls > xlsmetacrawler_results.txt
-./exiftool *.xlsx > xslxmetacrawler_results.txt
-./exiftool *.ppt > pptmetacrawler_results.txt
-./exiftool *.pptx > pptxmetacrawler_results.txt
-./exiftool *.csv > csvmetacrawler_results.txt
-./exiftool *.rtf > rtfmetacrawler_results.txt
-./exiftool *.zip > zipmetacrawler_results.txt
-./exiftool *.rtf > rtfmetacrawler_results.txt
-./exiftool *rar > rarmetacrawler_results.txt
-./exiftool *.tgz > tgzmetacrawler_results.txt
-./exiftool *.gz > gzmetacrawler_results.txt
-./exiftool *.xz > xzmetacrawler_results.txt
+mkdir downloaded_pdf_files
 mkdir metacrawler_results
-cp *.pdf /metacrawler_results/
-cp *.svg /metacrawler_results/
-cp *.doc /metacrawler_results/
-cp *.xls /metacrawler_results/
-cp *.xlsx /metacrawler_results/
-cp *.ppt /metacrawler_results/
-cp *.pptx /metacrawler_results/
-cp *.csv /metacrawler_results/
-cp *.rtf /metacrawler_results/
-cp *.tgz /metacrawler_results/
-cp *.gz /metacrawler_results/
-cp *.xz /metacrawler_results/
-cd ..
+cd exiftool
+./exiftool *.pdf > ./metacrawler_results/pdfmetacrawler_results.txt
+./exiftool *.svg > ./metacrawler_results/svgmetacrawler_results.txt
+./exiftool *.doc > ./metacrawler_results/docmetacrawler_results.txt
+./exiftool *.xls > ./metacrawler_results/xlsmetacrawler_results.txt
+./exiftool *.xlsx > ./metacrawler_results/xslxmetacrawler_results.txt
+./exiftool *.ppt > ./metacrawler_results/pptmetacrawler_results.txt
+./exiftool *.pptx > ./metacrawler_results/pptxmetacrawler_results.txt
+./exiftool *.csv > ./metacrawler_results/csvmetacrawler_results.txt
+./exiftool *.rtf > ./metacrawler_results/rtfmetacrawler_results.txt
+./exiftool *.zip > ./metacrawler_results/zipmetacrawler_results.txt
+./exiftool *.rtf > ./metacrawler_results/rtfmetacrawler_results.txt
+./exiftool *rar > ./metacrawler_results/rarmetacrawler_results.txt
+./exiftool *.tgz > ./metacrawler_results/tgzmetacrawler_results.txt
+./exiftool *.gz > ./metacrawler_results/gzmetacrawler_results.txt
+./exiftool *.xz > ./metacrawler_results/xzmetacrawler_results.txt
+cp *.pdf ./downloaded_pdf_files/
+cp *.svg ./downloaded_pdf_files/
+cp *.doc ./downloaded_pdf_files/
+cp *.xls ./downloaded_pdf_files/
+cp *.xlsx ./downloaded_pdf_files/
+cp *.ppt ./downloaded_pdf_files/
+cp *.pptx ./downloaded_pdf_files/
+cp *.csv ./downloaded_pdf_files/
+cp *.rtf ./downloaded_pdf_files/
+cp *.tgz ./downloaded_pdf_files/
+cp *.gz ./downloaded_pdf_files/
+cp *.xz ./downloaded_pdf_files/
 rm *.pdf
 rm *.svg
 rm *.doc
@@ -42,3 +42,4 @@ rm *.rtf
 rm *.tgz
 rm *.gz
 rm *.xz
+cd ..
