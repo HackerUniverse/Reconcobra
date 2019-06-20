2517c1df6b204e5a8f0deaf67147fd913d11b7bd
Haroon Awan <39236904+haroonawanofficial@users.noreply.github.com>
2019-06-21 01:57:06 +0500

    Add files via upload

--- endpoint_extraction.sh
+++ endpoint_extraction.sh
@@ -2,5 +2,5 @@
 mkdir endpoint_extraction_results
 cd endpoint_extraction_results
 echo -n "Enter URL to search : " ; read URL
-curl 'http://web.archive.org/cdx/search/cdx?url=$URL/*&output=text&fl=original&collapse=urlkey' > $URL.list
+curl "http://web.archive.org/cdx/search/cdx?url=$URL/*&output=text&fl=original&collapse=urlkey" > $URL.list
 cd ..
