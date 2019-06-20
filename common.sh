e2f8178f648b0e23114e8d75763d1d1fc18a81d1
Haroon Awan <39236904+haroonawanofficial@users.noreply.github.com>
2019-06-21 00:38:42 +0500

    Delete common.sh

--- common.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-mkdir common_crawl_results
-cd common_crawl_results
-echo -e "[ + ] URL Examples, com.google, pk.daraz " 
-echo -n "[ + ] Enter URL in reverse order only: " ; read Domain
-curl http://urlsearch.commoncrawl.org/download?q=$Domain -o $Domain.list
-cd ..
