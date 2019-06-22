f71f9f1c015bf745128bde383347004eece7d44f
Haroon Awan <39236904+haroonawanofficial@users.noreply.github.com>
2019-06-22 22:16:53 +0500

    Update linkfinder3.sh

--- linkfinder3.sh
+++ linkfinder3.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 cd LinkFinder
-echo "[ + ] Before using this tool"
-echo "[ + ] copy all js files into LinkFinder/jsfiles folder first"
-echo "[ + ] Otherwise you output will be an error"
 echo ""
-python linkfinder.py -i 'jsfiles/*.js' -r ^/api/ -o results.html
+echo "[ + ] Before using this tool"
+echo "[ + ] copy all js files into LinkFinder folder"
+echo "[ + ] loading results"
+echo ""
+python linkfinder.py -i '*.js' -r ^/api/ -o results.html
