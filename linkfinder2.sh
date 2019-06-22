63b92e2906513039ebbbccebf2b8a8cebcd90ea0
Haroon Awan <39236904+haroonawanofficial@users.noreply.github.com>
2019-06-22 18:29:12 +0500

    Update linkfinder2.sh

--- linkfinder2.sh
+++ linkfinder2.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+mkdir js_linkfinder
+cd js_linkfinder
 echo "[ + ] Enter TERMUX option for Termux else press RETURN :"
 read $first
 
@@ -8,12 +10,13 @@ then
 
 read -p "[ + ] Enter victim url: " victim
 echo "$victim"
-python js_linkfinder.py --wait=2 --download $victim
+python js_linkfinder.py --wait=2 --download https://$victim
 
 else
 
 read -p "[ + ] Enter victim url: " victim
 echo "$victim"
-python3 js_linkfinder.py --wait=2 --download $victim
+python3 js_linkfinder.py --wait=2 --download https://$victim
 
 fi
+cd ..
