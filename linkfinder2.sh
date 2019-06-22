e9ce1c9753e53ff43001cc949668a89258761f01
Haroon Awan <39236904+haroonawanofficial@users.noreply.github.com>
2019-06-22 18:44:55 +0500

    Update linkfinder2.sh

--- linkfinder2.sh
+++ linkfinder2.sh
@@ -12,13 +12,13 @@ then
 
 read -p "[ + ] Enter victim url: " victim
 echo "$victim"
-python js_linkfinder.py --wait=2 --download https://$victim
+python js_linkfinder.py --wait=2 --download $victim
 
 else
 
 read -p "[ + ] Enter victim url: " victim
 echo "$victim"
-python3 js_linkfinder.py --wait=2 --download https://$victim
+python3 js_linkfinder.py --wait=2 --download $victim
 
 fi
 
