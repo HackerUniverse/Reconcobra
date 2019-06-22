78ce32a396d8f04f88633c6c176bcc3e58fa7082
Haroon Awan <39236904+haroonawanofficial@users.noreply.github.com>
2019-06-22 19:13:26 +0500

    Update linkfinder2.sh

--- linkfinder2.sh
+++ linkfinder2.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 cd LinkFinder
-
 read -p "[ + ] Enter victim url: " victim
-echo "[ + ] Example: https://www.google.com and result will automatically available at browser
 echo "$victim"
 python2 linkfinder.py -i $victim -d
