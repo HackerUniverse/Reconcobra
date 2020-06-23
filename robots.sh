288a47ec7ac244df349c911f79dd41c6696694f6
Hacker_Universe <41442211+HackerUniverse@users.noreply.github.com>
2020-06-23 14:14:35 +0530

    updated

--- robots.sh
+++ robots.sh
@@ -12,5 +12,5 @@ RESET='\e[0m'
 echo "Enter Target"
 read Target
 # LOAD WEBSITE IN A WEB BROSER
-$BROWSER "https://www.google.com/search?q=site:$Target+ext:xml+|+ext:conf+|+ext:cnf+|+ext:reg+|+ext:inf+|+ext:rdp+|+ext:cfg+|+ext:txt+|+ext:ora+|+ext:ini" 2> /dev/null
+$BROWSER "https://www.google.com/search?q=site:$Target+ext:txt+robots.txt" 2> /dev/null
 
